3b660468260fbb05a61f7676e3ab6a7bc500d8c9
Giovanni Emanuele Nocco <giovanni.nocco@gmail.com>
2021-11-24 21:56:57 +0100

    updated pack

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -2,7 +2,7 @@
 echo Executing after success scripts on branch $TRAVIS_BRANCH
 echo Triggering NuGet package build
 
-dotnet pack -p:NuspecFile=.\Genocs.Core.nuspec --no-restore -o .
+dotnet pack --no-restore -o .
 
 echo Uploading Genocs.Core package to NuGet using branch $TRAVIS_BRANCH
 
