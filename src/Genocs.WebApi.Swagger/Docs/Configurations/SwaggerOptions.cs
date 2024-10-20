3ea5a05522ec98715802cd24ed1c660acd89588a
Nocco Giovanni Emanuele <giovanni.nocco@gmail.com>
2024-10-20 11:16:23 +0200

    Add detailed Swagger configuration and options
    Updated `appsettings.json` to include a comprehensive "swagger" section with configuration options such as `enabled`, `title`, `description`, `version`, `termsOfService`, `contactName`, `contactEmail`, `contactUrl`, `licenseName`, `licenseUrl`, `includeSecurity`, and a list of `servers`.
    
    Extended `SwaggerOptions` class to include new properties: `Description`, `ContactName`, `ContactEmail`, `ContactUrl`, `LicenseName`, `LicenseUrl`, `TermsOfService`, and a list of `OpenApiServer` objects. Added an inner class `OpenApiServer` to represent server details.
    
    Modified `AddSwaggerDocs` method in `Extensions.cs` to handle the new `SwaggerOptions` properties, including logic for security definitions, custom operation IDs, and server details. Added XML comments to the Swagger documentation.
    
    Provided specific values for the "swagger" section in `appsettings.json` for different environments (local development, containerized, and production).

--- src/Genocs.WebApi.Swagger/Docs/Configurations/SwaggerOptions.cs
+++ src/Genocs.WebApi.Swagger/Docs/Configurations/SwaggerOptions.cs
@@ -7,7 +7,23 @@ public class SwaggerOptions
     public string? Name { get; set; }
     public string? Title { get; set; }
     public string? Version { get; set; }
+    public string? Description { get; set; }
     public string? RoutePrefix { get; set; }
+    public string? ContactName { get; set; }
+    public string? ContactEmail { get; set; }
+    public string? ContactUrl { get; set; }
+    public string? LicenseName { get; set; }
+    public string? LicenseUrl { get; set; }
+    public string? TermsOfService { get; set; }
     public bool IncludeSecurity { get; set; }
     public bool SerializeAsOpenApiV2 { get; set; }
+    public List<OpenApiServer>? Servers { get; set; }
+
+    public class OpenApiServer
+    {
+
+        public string? Url { get; set; }
+        public string? Description { get; set; }
+    }
 }
+
