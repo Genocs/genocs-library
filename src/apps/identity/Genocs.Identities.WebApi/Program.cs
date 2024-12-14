23aa7c6418365f49be9de771be028dae7758c02a
Nocco Giovanni Emanuele <giovanni.nocco@gmail.com>
2024-12-14 19:08:38 +0100

    Enhance GenocsBuilder and update Program.cs structure
    - Added `WebApplicationBuilder` property to `GenocsBuilder` with XML documentation.
    - Refactored constructors to initialize `_buildActions` as an empty array.
    - Introduced a new `Build` method in `GenocsBuilder` for service provider construction.
    - Cleaned up unused namespaces in `Program.cs`.
    - Commented out default authorization policies and replaced with `UseCore()` for core services setup.
    - Enhanced endpoint mapping to support health checks and added `UseDispatcherEndpoints` for sign-in actions.

--- src/apps/identity/Genocs.Identities.WebApi/Program.cs
+++ src/apps/identity/Genocs.Identities.WebApi/Program.cs
@@ -1,19 +1,14 @@
-using System.Security.Claims;
 using Genocs.Auth;
 using Genocs.Core.Builders;
-using Genocs.Discovery.Consul;
 using Genocs.Identities.Application;
 using Genocs.Identities.Application.Commands;
 using Genocs.Identities.Application.Domain.Constants;
 using Genocs.Identities.Application.DTO;
 using Genocs.Identities.Application.Queries;
 using Genocs.Identities.Application.Services;
-using Genocs.LoadBalancing.Fabio;
 using Genocs.Logging;
-using Genocs.Secrets.Vault;
 using Genocs.WebApi;
 using Genocs.WebApi.CQRS;
-using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Serilog;
 
 StaticLogger.EnsureInitialized();
@@ -31,19 +26,13 @@ IGenocsBuilder gnxBuilder = await builder
 
 gnxBuilder.Build();
 
-//// Override the default authorization policy
-//builder.Services.AddAuthorizationBuilder()
-//                .AddPolicy("Reader", builder => builder.RequireAssertion(context => context.User.HasClaim(ClaimTypes.Role, "user")))
-//                .AddPolicy("Reader2", builder => builder.RequireClaim(ClaimTypes.Role, "user"))
-//                .AddPolicy("Reader3", builder => builder.RequireRole(["user"]))
-//                .AddPolicy("Reader4", builder => builder.AddRequirements(new AssertionRequirement(context => context.User.IsInRole("user"))))
-//                ;
-
 var app = builder.Build();
 
-// app.MapDefaultEndpoints();
-
+// Setup the Core services. The order matters, please keep it as is.
 app.UseCore();
+
+// Map default endpoints to provide support for health checks
+app.MapDefaultEndpoints();
 
 app.UseDispatcherEndpoints(endpoints => endpoints
                             .Post<SignIn>("sign-in", afterDispatch: (cmd, ctx) =>
