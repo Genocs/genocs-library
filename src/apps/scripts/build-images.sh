55dbec4b3abd1d9623243920d9161b0c7e2095e0
Giovanni Emanuele Nocco <giovanni.nocco@gmail.com>
2025-11-15 15:00:18 +0100

    chore: Update Dockerfiles and build script for improved structure and consistency

--- src/apps/scripts/build-images.sh
+++ src/apps/scripts/build-images.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-cd ./src/apps
-
 # Set environment variables coming from .env file
 export $(grep -v '^#' ./local.env | xargs)
 
-
 # Build the api gateway
-docker build -t genocs/apigateway:$IMAGE_VERSION -t genocs/apigateway:latest -f ./apigateway/WebApi/Dockerfile .
+docker build -t genocs/apigateway:$IMAGE_VERSION -t genocs/apigateway:latest -f ./src/apps/apigateway/WebApi/Dockerfile .
 
 # Build the identities service
-docker build -t genocs/identities-webapi:$IMAGE_VERSION -t genocs/identities-webapi:latest -f ./identities/WebApi/Dockerfile .
+docker build -t genocs/identities-webapi:$IMAGE_VERSION -t genocs/identities-webapi:latest -f ./src/apps/identities/WebApi/Dockerfile .
 
 # Build the products service
-docker build -t genocs/products-webapi:$IMAGE_VERSION -t genocs/products-webapi:latest -f ./products/WebApi/Dockerfile .
+docker build -t genocs/products-webapi:$IMAGE_VERSION -t genocs/products-webapi:latest -f ./src/apps/products/WebApi/Dockerfile .
 
 # Build the orders service
-docker build -t genocs/orders-webapi:$IMAGE_VERSION -t genocs/orders-webapi:latest -f ./orders/WebApi/Dockerfile .
+docker build -t genocs/orders-webapi:$IMAGE_VERSION -t genocs/orders-webapi:latest -f ./src/apps/orders/WebApi/Dockerfile .
 
 # Build the notifications service
-docker build -t genocs/notifications-webapi:$IMAGE_VERSION -t genocs/notifications-webapi:latest -f ./notifications/WebApi/Dockerfile .
-
-cd ..
-cd ..
+docker build -t genocs/notifications-webapi:$IMAGE_VERSION -t genocs/notifications-webapi:latest -f ./src/apps/notifications/WebApi/Dockerfile .
