b0e714475c21d8f403a152be92f479b78e2be2f4
Giovanni Emanuale Nocco <giovanni.nocco@gmail.com>
2026-02-08 21:44:38 +0100

    Update Certificate package version for net8.0 target
    Changed Microsoft.AspNetCore.Authentication.Certificate package reference from 8.1.* to 8.0.* for net8.0 target framework in Genocs.WebApi.Security.csproj.

--- src/Genocs.Metrics/AppMetrics/Extensions.cs
+++ src/Genocs.Metrics/AppMetrics/Extensions.cs
@@ -13,7 +13,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.ComponentModel;
 
 namespace Genocs.Metrics.AppMetrics;
 
