062b0c5de142c722645ba17dbf44536e94634714
Giovanni Emanuele Nocco <giovanni.nocco@hotmail.com>
2026-07-16 22:46:02 +0200

    updated packages in preparation for release 9.0.1

--- src/Genocs.Persistence.MongoDB/Configurations/IMongoOptionsBuilder.cs
+++ src/Genocs.Persistence.MongoDB/Configurations/IMongoOptionsBuilder.cs
@@ -8,22 +8,22 @@ public interface IMongoOptionsBuilder
     /// <summary>
     /// Setup the Connection string.
     /// </summary>
-    /// <param name="connectionString"></param>
-    /// <returns></returns>
+    /// <param name="connectionString">The connection string.</param>
+    /// <returns>The current instance of <see cref="IMongoOptionsBuilder"/>.</returns>
     IMongoOptionsBuilder WithConnectionString(string connectionString);
 
     /// <summary>
     /// Setup the Database name.
     /// </summary>
-    /// <param name="database"></param>
-    /// <returns></returns>
+    /// <param name="database">The database name.</param>
+    /// <returns>The current instance of <see cref="IMongoOptionsBuilder"/>.</returns>
     IMongoOptionsBuilder WithDatabase(string database);
 
     /// <summary>
     /// Setup the database Seed.
     /// </summary>
-    /// <param name="seed"></param>
-    /// <returns></returns>
+    /// <param name="seed">Indicates whether to seed the database.</param>
+    /// <returns>The current instance of <see cref="IMongoOptionsBuilder"/>.</returns>
     IMongoOptionsBuilder WithSeed(bool seed);
 
     /// <summary>
--- src/Genocs.Persistence.MongoDB/Repositories/Pagination.cs
+++ src/Genocs.Persistence.MongoDB/Repositories/Pagination.cs
@@ -7,44 +7,45 @@ namespace Genocs.Persistence.MongoDB.Repositories;
 
 public static class Pagination
 {
-    public static async Task<PagedResult<T>> PaginateAsync<T>(this IQueryable<T> collection, IPagedQuery query)
-        => await collection.PaginateAsync(query.OrderBy, query.SortOrder, query.Page, query.Results);
+    public static async Task<PagedResult<T>> PaginateAsync<T>(this IQueryable<T> collection, IPagedQuery query, CancellationToken cancellationToken = default)
+        => await collection.PaginateAsync(query.OrderBy, query.SortOrder, query.Page, query.Results, cancellationToken);
 
     public static async Task<PagedResult<T>> PaginateAsync<T>(
                                                               this IQueryable<T> collection,
                                                               string? orderBy,
                                                               string? sortOrder,
                                                               int page,
-                                                              int resultsPerPage)
+                                                              int resultsPerPage,
+                                                              CancellationToken cancellationToken = default)
     {
         if (resultsPerPage < 0)
         {
             resultsPerPage = 10;
         }
 
-        bool isEmpty = !await collection.AnyAsync();
+        bool isEmpty = !await collection.AnyAsync(cancellationToken);
         if (isEmpty)
         {
             return PagedResult<T>.Empty;
         }
 
-        int totalResults = await collection.CountAsync();
+        int totalResults = await collection.CountAsync(cancellationToken);
         int totalPages = (int)Math.Ceiling((decimal)totalResults / resultsPerPage);
 
         List<T> data;
         if (string.IsNullOrWhiteSpace(orderBy))
         {
-            data = await collection.Limit(page, resultsPerPage).ToListAsync();
+            data = await collection.Limit(page, resultsPerPage).ToListAsync(cancellationToken);
             return PagedResult<T>.Create(data, page, resultsPerPage, totalPages, totalResults);
         }
 
         if (sortOrder?.ToLowerInvariant() == "asc")
         {
-            data = await collection.OrderBy(ToLambda<T>(orderBy)).Limit(page, resultsPerPage).ToListAsync();
+            data = await collection.OrderBy(ToLambda<T>(orderBy)).Limit(page, resultsPerPage).ToListAsync(cancellationToken);
         }
         else
         {
-            data = await collection.OrderByDescending(ToLambda<T>(orderBy)).Limit(page, resultsPerPage).ToListAsync();
+            data = await collection.OrderByDescending(ToLambda<T>(orderBy)).Limit(page, resultsPerPage).ToListAsync(cancellationToken);
         }
 
         return PagedResult<T>.Create(data, page, resultsPerPage, totalPages, totalResults);
